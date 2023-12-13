3f58ea7ddc11a42467b44640b4f74a989e8f09b9
dljsgsg <153618446+dljsgsg@users.noreply.github.com>
2023-12-13 15:34:41 +0300

    Add files via upload

---
+++ hhdjbud.py
@@ -0,0 +1,23 @@
+#1
+a, b= int(input("введите число 1:")), int(input("ведите число 2:"))
+print(f"произведение чисел: {a * b}")
+#2
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num3 = int(input("Введите третье число: "))
+sum = num1 + num2 + num3
+prod = num1 * num2 * num3
+avg = sum / 3
+
+print("Сумма чисел:", sum)
+print("Произведение чисел:", prod)
+print("Среднее арифметическое чисел:", avg)
+#3
+a= int(input("диагональ №1:"))
+b= int(input("диагональ №2:"))
+
+print(f"произведение чисел: {a * b}")
+#4
+print("\nTo be \n or not \n to be")
+#5
+print("“Life is what happens" "\n                      when"   "\n                    you’re busy making other plans”  \n John Lennon.")
